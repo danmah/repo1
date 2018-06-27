4751dda5b1c4809cf96b9bd28d659b7aedb65740
danmah <daniel.maher@yahoo.com>
2018-06-27 00:30:43 -0400

    6e passe

--- someList2.py
+++ someList2.py
@@ -20,6 +20,7 @@ pos = [n for n in mylist if n > 0]
 
 print(pos)
 
+# Generator (au lieu d'une liste, donc () au lieu de [])
 pos2 = (n for n in mylist if n > 0)
 
 print(pos2)
@@ -36,14 +37,14 @@ print(ivals)
 
 # Keeping the items for which satisfy a second list criteria
 addresses = [
-    '5412 N CLARK',
     '5148 N CLARK',
-    '5800 E 58TH',
-    '2122 N CLARK'
     '5645 N RAVENSWOOD',
     '1060 W ADDISON',
     '4801 N BROADWAY',
     '1039 W GRANVILLE',
+    '5800 E 58TH',
+    '5412 N CLARK',
+    '2122 N CLARK'
 ]
 
 countpos = [0, 3, 10, 4, 1, 7, 6, 1]
